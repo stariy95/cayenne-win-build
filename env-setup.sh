8ba98e4b20d799167dfc4d40551b958549b36017
Nikita Timofeev <stariy95@gmail.com>
2019-04-10 14:44:50 +0300

    Initial setup of win assembly

--- env-setup.sh
+++ env-setup.sh
@@ -8,8 +8,8 @@ choco install jdk8
 choco install maven
 
 # TODO: can this be done by chocolatey? or something else more robust?
-JAVA_HOME="C:\\Program\ Files\\Java\\jdk1.8.0_201\\"
+JAVA_HOME="/c/Program Files/Java/jdk1.8.0_201/"
 
 # check
-\"${JAVA_HOME}\\bin\\java\" -version
-\"C:\\ProgramData\\chocolatey\\bin\\mvn\" -v
+"${JAVA_HOME}bin/java" -version
+/C/ProgramData/chocolatey/bin/mvn -v
