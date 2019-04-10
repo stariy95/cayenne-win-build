4e725cd6551799ffe198207f4d30cecd119dee94
Nikita Timofeev <stariy95@gmail.com>
2019-04-10 13:54:05 +0300

    Initial setup of win assembly

--- env-setup.sh
+++ env-setup.sh
@@ -10,5 +10,8 @@ choco install maven
 # TODO: can this be done by chocolatey? or something else more robust?
 JAVA_HOME="C:\\Program Files\\Java\\jdk1.8.0_201\\"
 
+echo ${JAVA_HOME}
+java -version
+
 # check
 mvn -v
