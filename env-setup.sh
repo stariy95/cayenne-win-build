89541133c28d2b7349e45295e685512714f98125
Nikita Timofeev <stariy95@gmail.com>
2019-04-10 15:01:42 +0300

    Maven wrapper

--- env-setup.sh
+++ env-setup.sh
@@ -7,7 +7,7 @@
 choco install jdk8
 
 # TODO: can this be done by chocolatey? or something else more robust?
-JAVA_HOME="/c/Program Files/Java/jdk1.8.0_201"
+export JAVA_HOME="/c/Program Files/Java/jdk1.8.0_201"
 
 # check
 "${JAVA_HOME}/bin/java" -version
