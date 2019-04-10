bb4ad377e78bc2e3dda3aaf5ac253ae952d9c8b4
Nikita Timofeev <stariy95@gmail.com>
2019-04-10 16:11:42 +0300

    Process errors

--- build.sh
+++ build.sh
@@ -21,6 +21,12 @@ mkdir "$BUILD_DIR"
 echo "Extracting sources..."
 tar -C ${BUILD_DIR} -xvzf cayenne-src.tar.gz > /dev/null 2>&1
 
+status=$?
+if [[ ${status} -ne 0 ]]; then
+    echo "Unable to extract sources"
+    exit ${status}
+fi
+
 SRC_DIR=`ls ${BUILD_DIR}`
 echo "Source directory: $SRC_DIR"
 cd  "$BUILD_DIR/$SRC_DIR"
@@ -28,8 +34,21 @@ cd  "$BUILD_DIR/$SRC_DIR"
 echo "Running Maven build... it can take a while..."
 ../../mvnw install -Passembly,windows -q -DskipTests -DcayenneLogLevel=ERROR -Duser.name=n.timofeev
 
+status=$?
+if [[ ${status} -ne 0 ]]; then
+    echo "Maven build failed"
+    exit ${status}
+fi
+
 cd "$BUILD_DIR/$SRC_DIR/assembly/target"
 ASSEMBLY=`ls -p | grep win.zip\$`
 
 mkdir ${BUILD_DIR}/assembly/
 cp ${ASSEMBLY} ${BUILD_DIR}/assembly/
+
+status=$?
+if [[ ${status} -ne 0 ]]; then
+    exit ${status}
+fi
+
+exit 0
