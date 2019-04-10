26e5622206ddae49ef28d4d06cafdb62051297bb
Nikita Timofeev <stariy95@gmail.com>
2019-04-10 14:55:38 +0300

    Maven wrapper

--- build.sh
+++ build.sh
@@ -25,7 +25,7 @@ echo "Source directory: $SRC_DIR"
 cd  "$BUILD_DIR/$SRC_DIR"
 
 echo "Running Maven build... it can take a while..."
-mvn install -Passembly,windows -q -DskipTests -Duser.name=n.timofeev > /dev/null 2>&1
+./mvnw.cmd install -Passembly,windows -q -DskipTests -Duser.name=n.timofeev > /dev/null 2>&1
 
 cd "$BUILD_DIR/$SRC_DIR/assembly/target"
 ASSEMBLY=`ls -p | grep win.zip\$`
--- env-setup.sh
+++ env-setup.sh
@@ -5,11 +5,10 @@
 #
 
 choco install jdk8
-choco install maven
 
 # TODO: can this be done by chocolatey? or something else more robust?
-JAVA_HOME="/c/Program Files/Java/jdk1.8.0_201/"
+JAVA_HOME="/c/Program Files/Java/jdk1.8.0_201"
 
 # check
-"${JAVA_HOME}bin/java" -version
-/C/ProgramData/chocolatey/bin/mvn -v
+"${JAVA_HOME}/bin/java" -version
+./mvnw.cmd -v
